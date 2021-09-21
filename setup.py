887aa407ea2a99bfe9fcee69e611fba6da469b15
Aria Ghora Prabono <hello@ghora.net>
2021-09-21 21:59:05 +0700

    Version bump

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name="asciinema_clip",
-    version="0.1.0",
+    version="0.1.2",
     author="Aria Ghora Prabono",
     author_email="hello@ghora.net",
+    description="Clip the displaying delay on asciicast produced by asciinema",
     license="MIT",
     packages=["asciinema_clip"],
     scripts=["bin/asciinema-clip"]
